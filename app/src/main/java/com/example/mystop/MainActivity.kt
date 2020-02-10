78346a9e6902b95129990d70c5618b02b96391ca
Oona <oona.e.rauhala@gmail.com>
2020-02-10 21:17:23 +0200

    Get user location and location permissions

--- app/src/main/java/com/example/mystop/MainActivity.kt
+++ app/src/main/java/com/example/mystop/MainActivity.kt
@@ -1,12 +1,63 @@
 package com.example.mystop
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
+    private var PERMISSION_FINE_LOCATION = 1
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //ask for location permission
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                                                != PackageManager.PERMISSION_GRANTED) {
+            //not granted, request
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                                                PERMISSION_FINE_LOCATION)
+        } else {
+            Log.d("TAG", "onCreate else")
+            getLocation()
+            //permission is granted
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            PERMISSION_FINE_LOCATION -> {
+                //if request is cancelled, the result array is empty
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    //permission was granted, do things with
+                    getLocation()
+                }
+                else {
+                    //permission denied
+                }
+            }
+        }
+    }
+
+    fun getLocation() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        fusedLocationClient.lastLocation.addOnCompleteListener(this) {
+            task ->
+            if (task.isSuccessful && task.result != null){
+                Log.d("TAG", "${task.result?.longitude}")
+            }
+        }
     }
 }
+
